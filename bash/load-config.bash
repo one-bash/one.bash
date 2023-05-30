04e4b6eab937a785ddabf1e29bce8e89d890c0d8
ADoyle <adoyle.h@gmail.com>
2023-05-30 17:55:16 +0800

    fix(one link): ONE_CONF: unbound variable

--- bash/load-config.bash
+++ bash/load-config.bash
@@ -3,7 +3,7 @@ ONE_CONF=${ONE_CONF:-${XDG_CONFIG_HOME:-$HOME/.config}/one.bash/one.config.bash}
 # shellcheck source=../deps/one_l.bash
 . "$ONE_DIR/deps/one_l.bash"
 
-# shellcheck disable=1090
+# shellcheck source=~/.config/one.bash/one.config.bash
 [[ -f $ONE_CONF ]] && . "$ONE_CONF"
 
 # shellcheck source=../one.config.default.bash
