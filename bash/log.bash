1d29d026169d60297904ba0313ed1cfe0721fa1e
ADoyle <adoyle.h@gmail.com>
2023-06-10 04:20:05 +0800

    fix: log format

--- bash/log.bash
+++ bash/log.bash
@@ -4,26 +4,26 @@ log_err() {
   local tag=$1
   shift 1
   local msg="$*"
-  echo "$msg" >&2
-  printf "[%s][ERROR][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" &>> "$ONE_LOG_FILE"
+  printf "[%s][ERROR][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" | tee -a "$ONE_LOG_FILE" >&2
 }
 
 log_warn() {
   local tag=$1
   shift 1
   local msg="$*"
-  echo "$msg" >&2
-  printf "[%s][WARN][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" &>> "$ONE_LOG_FILE"
+  printf "[%s][WARN][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" | tee -a "$ONE_LOG_FILE" >&2
 }
 
 log_info() {
   local tag=$1
   shift 1
   local msg="$*"
-  echo "$msg"
-  printf "[%s][INFO][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" &>> "$ONE_LOG_FILE"
+  printf "[%s][INFO][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" | tee -a "$ONE_LOG_FILE"
 }
 
 log_verb() {
-  printf "[%s][VERB][$1] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$2" &>> "$ONE_LOG_FILE"
+  local tag=$1
+  shift 1
+  local msg="$*"
+  printf "[%s][VERB][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" | tee -a "$ONE_LOG_FILE"
 }
