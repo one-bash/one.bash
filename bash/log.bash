3ec0acd7b1e24ca6741838b24cab9e239b4006ef
ADoyle <adoyle.h@gmail.com>
2024-04-02 11:55:19 +0800

    fix: add color to logs

--- bash/log.bash
+++ bash/log.bash
@@ -4,26 +4,26 @@ log_err() {
   local tag=$1
   shift 1
   local msg="$*"
-  printf "[%s][ERROR][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" | tee -a "$ONE_LOG_FILE" >&2
+  printf "%b[%s][ERROR][$tag] %s%b\n" "$RED" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" "$RESET_ALL" | tee -a "$ONE_LOG_FILE" >&2
 }
 
 log_warn() {
   local tag=$1
   shift 1
   local msg="$*"
-  printf "[%s][WARN][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" | tee -a "$ONE_LOG_FILE" >&2
+  printf "%b[%s][WARN][$tag] %s%b\n" "$YELLOW" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" "$RESET_ALL" | tee -a "$ONE_LOG_FILE" >&2
 }
 
 log_info() {
   local tag=$1
   shift 1
   local msg="$*"
-  printf "[%s][INFO][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" | tee -a "$ONE_LOG_FILE" >&2
+  printf "[%s][INFO][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" "$RESET_ALL" | tee -a "$ONE_LOG_FILE" >&2
 }
 
 log_verb() {
   local tag=$1
   shift 1
   local msg="$*"
-  printf "[%s][VERB][$tag] %s\n" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" | tee -a "$ONE_LOG_FILE" >&2
+  printf "%b[%s][VERB][$tag] %s%b\n" "$GREY" "$(date +"%Y-%m-%d %H:%M:%S")" "$msg" "$RESET_ALL" | tee -a "$ONE_LOG_FILE" >&2
 }
