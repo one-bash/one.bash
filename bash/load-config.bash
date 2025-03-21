323a35d8c2788933085bd40011ac4ca057c9f603
ADoyle <adoyle.h@gmail.com>
2025-03-22 03:55:17 +0800

    fix: ~/.config/one.bash/one.config.bash: No such file or directory
    When user invoke "one dep install" from scratch. It throw an error.

--- bash/load-config.bash
+++ bash/load-config.bash
@@ -4,10 +4,13 @@ ONE_CONF=${ONE_CONF:-${XDG_CONFIG_HOME:-$HOME/.config}/one.bash/one.config.bash}
 . "$ONE_DIR/deps/one_l.bash"
 
 # shellcheck source=../one.config.default.bash
-[[ -f $ONE_CONF ]] && . "$ONE_CONF"
-
-ONE_CONF_DIR=$(realpath "$ONE_CONF")
-ONE_CONF_DIR=${ONE_CONF_DIR%/*}
+if [[ -f $ONE_CONF ]]; then
+	. "$ONE_CONF"
+	ONE_CONF_DIR=$(realpath "$ONE_CONF")
+	ONE_CONF_DIR=${ONE_CONF_DIR%/*}
+else
+	ONE_CONF_DIR=${ONE_CONF%/*}
+fi
 
 # shellcheck source=../one.config.default.bash
 . "$ONE_DIR/one.config.default.bash"
